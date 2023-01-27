90370ddbea5bc33d6263012b9f3bfe87598b9e53
Anthony <crayonno.o@gmail.com>
2023-01-27 23:26:42 +0100

    'initio'

--- app/page.jsx
+++ app/page.jsx
@@ -1,91 +1,120 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import React from "react";
+import styles from "./loan.module.css";
+import { Ubuntu, Commissioner, Fresca } from "@next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const ubuntu = Ubuntu({ weight: "500" });
+const commissioner = Commissioner({ weight: "400" });
+const fresca = Fresca({ weight: "400" });
 
-export default function Home() {
+function loan() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.jsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.loan}>
+      <header>
+        <div className="logo">
+          <h1 className={ubuntu.className}>UND SERVICES</h1>
+        </div>
+
+        <nav className={styles.mobile}>
+          <ul>
+            <li className={fresca.className}>overview</li>
+            <li className={fresca.className}>features</li>
+            <li className={fresca.className}>forums</li>
+          </ul>
+        </nav>
+      </header>
+
+      <div className={styles.hero}>
+        <div className={styles.left}>
+          <h1 className={ubuntu.className}>
+            get a smarter <span>loan</span> from the comfort of your home
+          </h1>
+          <button>
+            <p className={ubuntu.className}>Check your rate</p>
+          </button>
+          <div className={styles.col2}>
+            <li className={commissioner.className}>Easy transparent</li>
+            <li className={commissioner.className}>For personal loans</li>
+            <li className={commissioner.className}>No repayment fees</li>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <img src="/me.jpg" alt="" />
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+      <div className={styles.apply}>
+        <h2 className={ubuntu.className}>
+          Apply in minutes. <br /> Money within days.
+        </h2>
+        <p className={ubuntu.className}>What would you like to do?</p>
+
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <img src="/bellBlack.svg" alt="" />
+            <div className={styles.text}>
+              <h3 className={ubuntu.className}>Pay off with credit cards</h3>
+              <p className={ubuntu.className}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Adipisci at earum architecto nobis expedita. Quidem harum quam
+                quae molestias rerum.
+              </p>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <img src="/bellBlack.svg" alt="" />
+            <div className={styles.text}>
+              <h3 className={ubuntu.className}>Consolidate debt</h3>
+              <p className={ubuntu.className}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Adipisci at earum architecto nobis expedita. Quidem harum quam
+                quae molestias rerum.
+              </p>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <img src="/bellBlack.svg" alt="" />
+            <div className={styles.text}>
+              <h3 className={ubuntu.className}>Something else</h3>
+              <p className={ubuntu.className}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Adipisci at earum architecto nobis expedita. Quidem harum quam
+                quae molestias rerum.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className={styles.fair}>
+        <h2 className={ubuntu.className}>
+          Fair and personal loans and car refinancing
+        </h2>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+
+
+        <section>
+          <div>
+            <p className={ubuntu.className}>
+              For personal loans, rates <span>are 43% lower</span> as compared
+              to a credit score-only model.
+            </p>
+          </div>
+          <div>
+            <img src="/bell.svg" alt="" />
+            <p className={ubuntu.className}> Easy, transparent process</p>
+          </div>
+          <div>
+            <img src="/bell.svg" alt="" />
+            <h2 className={ubuntu.className}>No payment fees</h2>
+            <p className={ubuntu.className}>
+              You may pay off part or all the loan at any time with no fee
+            </p>
+          </div>
+        </section>
       </div>
-    </main>
-  )
+    </div>
+  );
 }
+
+export default loan;
